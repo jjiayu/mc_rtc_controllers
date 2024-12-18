85693a3413b565ef345d3c5716558b6548b94240
Jiayi Wang <jjiayu889@gmail.com>
2024-12-18 13:43:54 +0800

    door opening code

--- my_first_controller/single_robot_controller.py
+++ my_first_controller/single_robot_controller.py
@@ -1,16 +1,10 @@
 import mc_control
-import mc_solver
 import mc_rbdyn
 import mc_rtc
 import mc_tasks
 import eigen
 import math
 import sva
-
-# Constants (enumerates)
-APPROACH = 0
-HANDLE = 1
-OPEN = 2
 
 class MyFirstController(mc_control.MCPythonController):
     def __init__(self, rm, dt):
@@ -45,13 +39,27 @@ class MyFirstController(mc_control.MCPythonController):
         #   for CoM task
         self.comDown = True
         self.comZero = eigen.Vector3d.Zero()
-        #   for door openning task
-        self.phase = APPROACH
 
-
-    # Call this in the run callback
+    # Controller call back for each control cycle, here we gitve targets to the tasks
     def run_callback(self):
-        self.switch_phase()
+        if (
+            abs(
+                self.postureTask.posture()[self.jointIndex][0]
+                - self.robot().mbc.q[self.jointIndex][0]
+            )
+            < 0.05
+        ):
+            self.switch_target()
+        
+        if self.comTask.eval().norm()<0.01:
+            self.switch_com_target()
+
+        # Get the current objective
+        pt = self.efTask.get_ef_pose()
+        # Update the rotation and position objective
+        self.efTask.set_ef_pose(sva.PTransformd(sva.RotY(-math.pi / 2), eigen.Vector3d(0.5, -0.5, 1.2))
+)
+    
         return True
     
     # Reset callback
@@ -62,39 +70,6 @@ class MyFirstController(mc_control.MCPythonController):
 
         # Reset end-effector task
         self.efTask.reset()
-
-        #Reset the door
-        self.robots().robot(1).posW(sva.PTransformd(sva.RotZ(math.pi), eigen.Vector3d(0.7, 0.5, 0)))
-        self.doorKinematics = mc_solver.KinematicsConstraint(self.robots(), 1, self.qpsolver.timeStep)
-        self.qpsolver.addConstraintSet(self.doorKinematics)
-        self.doorPosture = mc_tasks.PostureTask(self.qpsolver, 1, 5.0, 1000.0)
-        self.qpsolver.addTask(self.doorPosture)
-
-        # Reach the handle task
-        self.handTask = mc_tasks.SurfaceTransformTask("RightGripper", self.robots(), 0, 5.0, 1000.0)
-        self.qpsolver.addTask(self.handTask)
-        # Set a target relative to the handle position
-        self.handTask.target(sva.PTransformd(eigen.Vector3d(0, 0, -0.025))* self.robots().robot(1).surfacePose("Handle"))
-
-    # New method for our controller
-    def switch_phase(self):
-        if (self.phase == APPROACH and self.handTask.eval().norm() < 0.05 and self.handTask.speed().norm() < 1e-4):
-            # Add a new contact
-            self.addContact(self.robot().name(), "door", "RightGripper", "Handle")
-            # Remove the surface transform task
-            self.qpsolver.removeTask(self.handTask)
-            # Keep the robot in its current posture
-            self.postureTask.reset()
-            self.comTask.reset()
-            # Target new handle position
-            self.doorPosture.target({"handle".encode('utf-8'): {-1.0}})
-            # Switch phase
-            self.phase = HANDLE
-        elif self.phase == HANDLE and self.doorPosture.eval().norm() < 0.01:
-            # Update door opening target
-            self.doorPosture.target({"door".encode('utf-8'): [0.5]})
-            # Switch phase
-            self.phase = OPEN
 
     def switch_target(self):
         if self.goingLeft:
@@ -113,6 +88,5 @@ class MyFirstController(mc_control.MCPythonController):
     #load the controller
     @staticmethod
     def create(robot, dt):
-        door = mc_rbdyn.get_robot_module("env", mc_rtc.MC_ENV_DESCRIPTION_PATH.decode('utf-8') + "/../mc_int_obj_description", "door")
-        ground = mc_rbdyn.get_robot_module("env", mc_rtc.MC_ENV_DESCRIPTION_PATH, "ground")
-        return MyFirstController([robot, door, ground], dt) #the load order decides the index number of each object, the robot is always be 0
+        env = mc_rbdyn.get_robot_module("env", mc_rtc.MC_ENV_DESCRIPTION_PATH, "ground")
+        return MyFirstController([robot,env], dt)
