5301a36c0bba192ea2c1233534aefece60a08552
Jiayi Wang <jiayi.robotics@hotmail.com>
2024-12-23 11:24:21 +0800

    head shaking controller

---
+++ my_first_controller/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jiayu/Desktop/git_repos/mc_rtc_controllers/my_first_controller
+# Build directory: /home/jiayu/Desktop/git_repos/mc_rtc_controllers/my_first_controller/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ my_first_controller/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jiayu/Desktop/git_repos/mc_rtc_controllers/my_first_controller/src
+# Build directory: /home/jiayu/Desktop/git_repos/mc_rtc_controllers/my_first_controller/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
